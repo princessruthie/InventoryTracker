9547cd8e6f3f809802d511f1911b18c659b6880d
princessruthie <missruthie@gmail.com>
2016-07-13 16:41:15 -0400

    Add a model for Stock items.

---
+++ app/src/main/java/com/ruthiefloats/inventorytracker/model/Stock.java
@@ -0,0 +1,60 @@
+package com.ruthiefloats.inventorytracker.model;
+
+import android.graphics.Bitmap;
+
+public class Stock {
+    private String name;
+    private String description;
+    private int quantity;
+    private long id;
+    private int price;
+    private Bitmap mBitmap;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Bitmap getBitmap() {
+        return mBitmap;
+    }
+
+    public void setBitmap(Bitmap bitmap) {
+        mBitmap = bitmap;
+    }
+}
