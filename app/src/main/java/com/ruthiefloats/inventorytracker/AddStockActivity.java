cdad845603d90881fd593973987f8b849b7f9e33
princessruthie <missruthie@gmail.com>
2016-07-15 18:18:45 -0400

    Add basic functionality for the AddStockActivity.  Adjusted AddStockActivity’s layout.  Note the Activity currently adds a default photo.

--- app/src/main/java/com/ruthiefloats/inventorytracker/AddStockActivity.java
+++ app/src/main/java/com/ruthiefloats/inventorytracker/AddStockActivity.java
@@ -4,9 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.ruthiefloats.inventorytracker.model.Stock;
+import com.ruthiefloats.inventorytracker.tools.StocksDataSource;
+
 public class AddStockActivity extends AppCompatActivity {
+
+    private static final String TAG = "AddStockActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +32,30 @@ public class AddStockActivity extends AppCompatActivity {
         submitNewStockButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(AddStockActivity.this, "Submit New Stock Item", Toast.LENGTH_SHORT).show();
+                addStock();
             }
         });
+    }
+
+    private void addStock(){
+        EditText nameEditText = (EditText) findViewById(R.id.add_name);
+        EditText priceEditText = (EditText) findViewById(R.id.add_price);
+        EditText quantityEditText = (EditText) findViewById(R.id.add_quantity);
+
+        String name = String.valueOf(nameEditText.getText());
+        double price = Double.parseDouble(priceEditText.getText().toString());
+        int quantity = Integer.parseInt(quantityEditText.getText().toString());
+
+        Stock stock = new Stock();
+        stock.setName(name);
+        stock.setPrice(price);
+        stock.setQuantity(quantity);
+        //todo come back to actually vary this.
+        stock.setImage("park9");
+
+        StocksDataSource dataSource = new StocksDataSource(this);
+        dataSource.open();
+        dataSource.create(stock);
+        dataSource.close();
     }
 }
