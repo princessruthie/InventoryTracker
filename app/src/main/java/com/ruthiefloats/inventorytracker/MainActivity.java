31070dacb8f24337b1c7f7e210b52f4d6f9061db
princessruthie <missruthie@gmail.com>
2016-07-15 14:54:49 -0400

    Optimize imports.

--- app/src/main/java/com/ruthiefloats/inventorytracker/MainActivity.java
+++ app/src/main/java/com/ruthiefloats/inventorytracker/MainActivity.java
@@ -1,23 +1,18 @@
 package com.ruthiefloats.inventorytracker;
 
 import android.content.Intent;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ruthiefloats.inventorytracker.model.DummyData;
 import com.ruthiefloats.inventorytracker.model.Stock;
-import com.ruthiefloats.inventorytracker.tools.DBOpenHelper;
 import com.ruthiefloats.inventorytracker.tools.StocksDataSource;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
